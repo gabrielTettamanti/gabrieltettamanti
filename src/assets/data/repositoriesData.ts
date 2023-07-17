6b7f5b9f3f4cc1c12be427590b02549257be9323
Gabriel Tettamanti <102546178+gabrielTettamanti@users.noreply.github.com>
2023-07-17 19:41:58 -0300

    Actualizar repositoriesData.ts

--- src/assets/data/repositoriesData.ts
+++ src/assets/data/repositoriesData.ts
@@ -30,10 +30,7 @@ import TatetiSolidJS from '../../assets/images/repos/tatetisolid.png'
 import freeCodeCampSim from '../../assets/images/repos/freecodecampsimulation.png'
 import MercadoLiebre from '../../assets/images/repos/mercadoliebre.png'
 import ClicksSpeedTest from "../../assets/images/repos/clickspeedtest.png";
-import HelloCube from "../../assets/images/repos/hellocube.png";
 import ClickCounter from "../../assets/images/repos/clickcounter.png";
-import CameraThreeJS from "../../assets/images/repos/camerathreejs.png";
-import CalculatorPy from "../../assets/images/repos/pythoncalculator.png";
 import VueClickCounter from "../../assets/images/repos/vueclickcounter.png";
 
 const repositoriesData = [
@@ -227,37 +224,7 @@ const repositoriesData = [
     github: "https://github.com/gabrielTettamanti/react-click-counter",
     desktop: "https://reactclickcounter.vercel.app/",
     img: ClickCounter,
-  },
-  {
-    title: "Calculadora con Python",
-    description:
-      "Es una calculadora desarrollada con python que cuenta con un cliente para usarse como aplicación de escritorio.",
-    technologies: [pyLogo],
-    url: "",
-    github: "https://github.com/gabrielTettamanti/python-calculator",
-    desktop: "",
-    img: CalculatorPy,
-  },
-  {
-    title: "Cámaras ThreeJS",
-    description:
-      "Es mi primera prueba con la tecnología de Cámara de ThreeJS. Desde una perspectiva alejada se muestra una cámara con un campo de visión observando un cubo y un fondo más alejado.",
-    technologies: [jsLogo, htmlLogo, threeJSLogo, GitLogo],
-    url: "",
-    github: "https://github.com/gabrielTettamanti/cameraThreeJS",
-    desktop: "https://camerathreejs.vercel.app/",
-    img: CameraThreeJS,
-  },
-  {
-    title: "Hello Cube",
-    description:
-      "Es mi primera prueba usando la librería ThreeJS de Javascript. Esta es la herramienta más usada en el desarrollo de software 3D web.",
-    technologies: [jsLogo, htmlLogo, threeJSLogo, GitLogo],
-    url: "https://youtu.be/uOqDkRhXWZY",
-    github: "https://github.com/gabrielTettamanti/helloCubeThreeJS",
-    desktop: "https://hellocube.vercel.app/",
-    img: HelloCube,
-  },
+  }
 ];
 
 export default repositoriesData
