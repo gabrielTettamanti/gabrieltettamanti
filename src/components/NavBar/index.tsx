2cf273288defc40904e17f55ef19b5f55be9c709
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-07-18 16:26:47 -0300

    Changed flags imgs

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,8 +1,8 @@
 import './styles.css'
 import { inject } from '@vercel/analytics';
 
-// import ukFlag from "./../../assets/images/uk.png";
-// import spanishFlag from "./../../assets/images/spanish.png";
+// import ukFlag from "./../../assets/images/uk.webp";
+// import spanishFlag from "./../../assets/images/spanish.webp";
 
 function NavBar() {
   inject();
